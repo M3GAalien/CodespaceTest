4af97a2be54031312328cf343d42919ec0ca0dc9
M3GAalien <55897035+M3GAalien@users.noreply.github.com>
2025-11-18 01:39:05 +0000

    trying again to add options

--- Gaiia_Automation_Test/Program.cs
+++ Gaiia_Automation_Test/Program.cs
@@ -9,7 +9,7 @@ if(choice == 0)
 }
 
 string url = "https://app.gaiia.com/iq-fiber/accounts?filters=%7B";
-string option = choice == 1 ? """customTypeIds"%3A%5B"6421ab73-c467-4f79-ac17-ba91ad3ee0c4"%5D%7D""" : """customTypeIds"%3A%5B"919285ef-deb8-4c1b-8fc9-e36f6eb00932"%5D%7D""";
+string option = choice == 1 ? """\"customTypeIds"%3A%5B"6421ab73-c467-4f79-ac17-ba91ad3ee0c4"%5D%7D""" : """\"customTypeIds"%3A%5B"919285ef-deb8-4c1b-8fc9-e36f6eb00932"%5D%7D""";
 
 Process.Start(new ProcessStartInfo
 {
